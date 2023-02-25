e9358636ce9847ac5a3e44ab66f1770f730808b1
b-chen12 <62766692+b-chen12@users.noreply.github.com>
2023-02-24 23:57:35 -0600

    added some psuedocode

--- proj2.py
+++ proj2.py
@@ -37,7 +37,20 @@ class TurtleControl:
     def color_callback(self, msg):
         self.timer = self.node.create_timer(1, self.timer_callback)
         print("red:", msg.r, "green:",msg.g, "blue:", msg.b)
-
+        
+    def move2goal(self):
+        # query user for goal coordinate
+        goal_pose = Pose()
+        goal_pose.x = float(input("Set your x goal: "))
+        goal_pose.y = float(input("Set your y goal: "))
+        distance_tolerance = float(input("Set your tolerance: "))
+        
+        
+        # rotate the turtle to the desired angle
+        # stop rotating
+        
+        # move the turtle linearly
+        # stop moving
 
 def main(args=None):
     print("program started")
